a5ec6dd2e5088ed53a575bfe91de892aba8fac9a
Michael Mendioola <mendiola.67@osu.edu>
2018-10-11 13:26:25 -0400

    Initial commit

---
+++ symcode.java
@@ -0,0 +1,14 @@
+/*
+ * Symcode project for Symantec challange
+ * Given a string of ascii characters,
+ * return the equivalent symcode with padding.
+ * @author Michael Mendiola
+ */
+
+ public class Symcode{
+
+
+   public static void main(String[] args){
+
+   }
+ }
