3ba14da9b5b78ba57ecfe5863595a80d3b7aecc5
Michael Mendiola <mendiola.67@osu.edu>
2018-10-11 20:13:54 -0400

    update variables and documentation

--- src/Symcode.java
+++ src/Symcode.java
@@ -1,13 +1,14 @@
 /*
- * Symcode project for Symantec challenge
- * Given a string of ASCII characters,
- * return the equivalent symcode with padding.
- * @author Michael Mendiola
- */
+* Symcode project for Symantec challenge
+* Given a string of ASCII characters,
+* return the equivalent symcode with padding.
+* @author Michael Mendiola
+*/
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -16,105 +17,144 @@ public class Symcode{
 	final static int RADIX = 8;
 	final static int BITS = 3;
 	private static final Map<Integer, String> indexMap = new HashMap<Integer, String>() {{
-	    put(0, "s");
-	    put(1, "y");
-	    put(2, "m");
-	    put(3, "a");
-	    put(4, "n");
-	    put(5, "t");
-	    put(6, "e");
-	    put(7, "c");
+		put(0, "s");
+		put(1, "y");
+		put(2, "m");
+		put(3, "a");
+		put(4, "n");
+		put(5, "t");
+		put(6, "e");
+		put(7, "c");
 	}};
-	
+
 	public static void main(String[] args) throws IOException{
-		File file = new File("/Users/michaelmendiola/Documents/GitHub/Symcode/symcode_small_test.txt");
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String line; 
-		ArrayList<String> test = new ArrayList<>();
+		// Scan for input and output paths
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input file path:");
+		String inputPath = sc.nextLine();
+		System.out.println("Enter output file path:");
+		String outputPath = sc.nextLine();
+
+		File input = new File(inputPath);
+		BufferedReader br = new BufferedReader(new FileReader(input));
+
+		File output = new File(outputPath);
+		FileWriter fw = new FileWriter(output);
+
+		String line;
+		ArrayList<String> hex = new ArrayList<>();
 		while ((line = br.readLine()) != null) {
 			line = line.trim();
-			if (line.isEmpty()){
-				System.out.println();
-			}else {
-				test = textToHex(line);
-				String binary = hexToBits(test);
-				Object[] arr1 = bitsToIndex(binary);
-				ArrayList<Integer> shit = (ArrayList<Integer>) arr1[0];
-				int padding = (int) arr1[1];
-				String fuck = indexToCode(shit, padding);
-			}		
-		  } 
-		
-		//System.out.println(test.toString());
-		
+
+			if (line.isEmpty()) {
+				fw.write(System.lineSeparator());
+			} else {
+				hex = textToHex(line);
+				String binary = hexToBits(hex);
+
+				Object[] indices = bitsToIndex(binary);
+				ArrayList<Integer> keys = (ArrayList<Integer>) indices[0];
+				int padding = (int) indices[1];
+
+				String code = indexToCode(keys, padding);
+				fw.write(code);
+
+			}
+		}
+		fw.close();
+		System.out.println("Successfully encoded to: " + outputPath);
+
 
 	}
-	
+
 	/* Convert ASCII text to hex values
-	 * 
-	 * @param text
-	 * 		Text from input file
-	 * 
-	 * @returns hex
-	 * 		ArrayList of hex values for each char of @text
-	 */
+	*
+	* @param text
+	* 		Text from input file
+	*
+	* @returns hex
+	* 		ArrayList of hex values for each char of @text
+	*/
 	private static ArrayList<String> textToHex(String text){
 		ArrayList<String> hex = new ArrayList<>();
 		char[] chars = text.toCharArray();
 		for (char ch : chars) {
 			hex.add(Integer.toHexString((int) ch));
 		}
-		//System.out.println("Hex: " + hex.toString());
 		return hex;
 	}
 
 	/* Convert hex to 8 bit binary using Integer methods
-	 * 
-	 * @param hex
-	 * 		Text from input file
-	 * @returns bin
-	 * 		String from bin StringBuilder object
-	 * 
-	 */
+	*
+	* @param hex
+	* 		Text from input file
+	* @returns bin
+	* 		String from bin StringBuilder object
+	*
+	*/
 	private static String hexToBits(ArrayList<String> hex) {
 		StringBuilder bin = new StringBuilder();
 		for (String val : hex){
 			String s = Integer.toBinaryString(Integer.parseInt(val, 16));
 			int len = s.length();
+			// Add 0's so bin is 8 bits
 			bin.append(len == RADIX ? s : "00000000".substring(len) + s);
 		}
-		//System.out.println("Binary: " + bin.toString());
 		return bin.toString();
 	}
-	
+
+	/* Convert 3 bit binary to integer, add padding
+	*
+	* @param bin
+	* 		Binary String
+	* @returns indices
+	* 		Object array where indices[0] is an ArrayList of chars,
+	*     indices[1] is an int representing the number of padded 0's added
+	*/
 	private static Object[] bitsToIndex(String bin){
 		Object[] indices = new Object[2];
 		ArrayList<Integer> codeVals = new ArrayList<>();
 		int len = bin.length();
 		int padding = 0;
+
 		// To be use for substrings of bin
 		int start = 0; int end = 3;
+
+		// add 0's for padding
 		while (!(len % BITS == 0 && len % RADIX == 0)){
 			bin += "0";
 			len = bin.length();
 			padding++;
 		}
-		//System.out.println("Padded: " + bin + " # " + padding);
-		// Padding set to # of 0's added then reduced to 0's represented by '$'
+
+		// Padding set to # of 0's added, then reduced to 0's represented by '$'
 		padding -= padding % BITS;
 		len -= padding;
+
+		// Convert 3 bit substrings to ints, store in codeVals
 		while (end != len + BITS){
 			String sub = bin.substring(start, end);
 			codeVals.add(Integer.parseInt(sub, 2));
 			start += BITS;
 			end += BITS;
 		}
+
 		indices[0] = codeVals;
 		indices[1] = padding;
-		//System.out.println("Indices: " + indices[0].toString());
+
 		return indices;
 	}
 
+	/* Convert key to corresponding symcode and add $'s
+	*
+	* @param indices
+	* 		ArrayList of int keys to be checked against @indexMap
+	* @param padding
+	*     Number of padded 0's
+	* @returns code
+	* 		line of symcode
+	*
+	*/
 	private static String indexToCode(ArrayList<Integer> indices, int padding){
 		int numSymbols = padding / BITS;
 		StringBuilder code = new StringBuilder();
@@ -124,7 +164,7 @@ public class Symcode{
 		for (int i = 0; i < numSymbols; i++){
 			code.append("$");
 		}
-		System.out.println(code.toString());
+
 		return code.toString();
 	}
 
